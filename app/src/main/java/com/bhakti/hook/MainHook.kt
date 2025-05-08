0459a5f823c37838d8b29c976dcf0278a8292d5e
bhactea <45590138+bhactea@users.noreply.github.com>
2025-05-08 23:33:03 +0700

    Update MainHook.kt

--- app/src/main/java/com/bhakti/hook/MainHook.kt
+++ app/src/main/java/com/bhakti/hook/MainHook.kt
@@ -2,38 +2,30 @@ package com.bhakti.hook
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.util.Log
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
-import android.util.Log
 
 class MainHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName != "com.harpamobilehr") return
 
-        XposedHelpers.findAndHookConstructor(
-            WebViewClient::class.java,
+        Log.d("HOOK", "Harpa Mobile HR loaded!")
+
+        XposedHelpers.findAndHookMethod(
+            WebView::class.java,
+            "loadUrl",
+            String::class.java,
             object : XC_MethodHook() {
                 override fun afterHookedMethod(param: MethodHookParam) {
-                    val client = param.thisObject as WebViewClient
-
-                    XposedHelpers.findAndHookMethod(
-                        client.javaClass,
-                        "onPageFinished",
-                        WebView::class.java,
-                        String::class.java,
-                        object : XC_MethodHook() {
-                            override fun afterHookedMethod(param: MethodHookParam) {
-                                val webView = param.args[0] as WebView
-                                webView.evaluateJavascript(
-                                    "(function() { return document.documentElement.outerHTML; })();"
-                                ) { html ->
-                                    Log.d("HTML_DUMP", html ?: "HTML kosong")
-                                }
-                            }
-                        }
-                    )
+                    val webView = param.thisObject as WebView
+                    webView.evaluateJavascript(
+                        "(function() { return document.documentElement.outerHTML; })();"
+                    ) { html ->
+                        Log.d("HTML_DUMP", html ?: "HTML kosong")
+                    }
                 }
             }
         )
